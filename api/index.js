2f02471d0f9869e23d855edcfd409c7f6e0b8d9a
Ziadkarim84 <ziadkarim84@gmail.com>
2026-02-23 00:17:12 +0600

    debug: surface boot errors in API response

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-const { createApp } = require('../dist/app');
-module.exports = createApp();
+let app;
+try {
+  const { createApp } = require('../dist/app');
+  app = createApp();
+} catch (err) {
+  const express = require('express');
+  app = express();
+  app.use((_req, res) => {
+    res.status(500).json({ boot_error: err.message, stack: err.stack });
+  });
+}
+module.exports = app;
